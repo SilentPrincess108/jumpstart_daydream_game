adc85876cc4565654422076341ebb084ff24577f
Rosan <rosan.syal@gmail.com>
2025-08-25 19:48:34 -0400

    Added crappy jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,16 +2,20 @@ extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-const JUMP = 300.0
-const SPEED = 500.0
+const JUMP = -200.0
+const SPEED = 300.0
 
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var width = 1200/2
 var height = 700/2
 
+
 func _ready() -> void:
-	position = Vector2(width, height)
+	pass
 
 func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		position.y += gravity * delta
 	get_input(Input, delta)
 		
 func get_input(event, delta):
@@ -23,6 +27,8 @@ func get_input(event, delta):
 		position.x += SPEED * delta
 		animated_sprite.flip_h = false
 		animated_sprite.play("Run")
+	elif event.is_action_pressed("jump"):
+		position.y = JUMP
 	else:
 		animated_sprite.play("Idle")
 	move_and_slide()
