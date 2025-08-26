c5adada76cfc22c9bebac51098b7cbdc39ba02f8
Rosan <rosan.syal@gmail.com>
2025-08-26 13:18:23 -0400

    Created proper player movement with help of a tutorial

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,34 +1,47 @@
 extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@export_range(0, 1) var deceleration = 0.1
+@export_range(0, 1) var acceleration = 0.1
+@export_range(0, 1) var decelerate_on_jump_release = 0.5
 
-const JUMP = -200.0
+const JUMP = -400.0
 const SPEED = 300.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var width = 1200/2
 var height = 700/2
 
-
 func _ready() -> void:
 	pass
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
-		position.y += gravity * delta
-	get_input(Input, delta)
+		velocity.y += gravity * delta #Brings player down when if not on collidable object
+	get_input()
+	
+
+func get_input():
+	#Handle jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
+		animated_sprite.play("Jump")
+	
+	#Handle movement w/ accel/decel
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = move_toward(velocity.x, direction * SPEED, SPEED * acceleration) #acceleration
+		animated_sprite.play("Run")
 		
-func get_input(event, delta):
-	if event.is_action_pressed("left"):
-		position.x -= SPEED * delta
-		animated_sprite.flip_h = true
-		animated_sprite.play("Run")
-	elif event.is_action_pressed("right"):
-		position.x += SPEED * delta
-		animated_sprite.flip_h = false
-		animated_sprite.play("Run")
-	elif event.is_action_pressed("jump"):
-		position.y = JUMP
+		if direction >= 1:
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
 	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED * deceleration) #deceleration
 		animated_sprite.play("Idle")
+		
 	move_and_slide()
