3497678aa26858c77d59b9c516eb707f38e8febc
Rosan <rosan.syal@gmail.com>
2025-08-30 00:46:58 -0400

    Fixed cartridge text

--- scripts/cartridge_platform.gd
+++ scripts/cartridge_platform.gd
@@ -1,21 +1,25 @@
 extends AnimatableBody2D
 
 @export var game_name: String = "Temp Name"
-@export var desc: String = "Temp desc"
+@export var descrip: String = "Temp desc"
 @onready var label: Label = $Label
-@onready var player: CharacterBody2D = $"../player"
+@onready var desc: Label = $Desc
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	label.text = game_name
+	desc.text = descrip
 	label.hide()
+	desc.hide()
 
 func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
 	print("entered")
 	label.show()
+	desc.show()
 
 
 func _on_area_2d_body_exited(body: CharacterBody2D) -> void:
 	print("exited")
 	label.hide()
+	desc.show()
