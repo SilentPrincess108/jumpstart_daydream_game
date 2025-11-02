61d699e76a94092bc8b18814f1112bdded932137
Rosan <rosan.syal@gmail.com>
2025-11-02 15:39:51 -0500

    finished level screen

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -7,11 +7,11 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_screen.tscn")
 
 
 func _on_options_button_pressed() -> void:
